76dee26a51973d621c9366211b99b7d90347a602
Andrew Caetano <andrewr_caetano@hotmail.com>
2024-03-11 09:43:13 -0300

    Requisito 2

--- src/pages/Wallet.tsx
+++ src/pages/Wallet.tsx
@@ -1,11 +1,28 @@
-import store from '../redux';
+import React from 'react';
+import { useSelector } from 'react-redux';
 
 function Wallet() {
-  const { user } = store.getState();
+  const INITIAL_VALUE = 0;
+  const userEmail = useSelector((state: any) => state.user.email);
+
   return (
-    <div>
-      TrybeWallet
-    </div>
+    <>
+      <div>
+        TrybeWallet
+      </div>
+      <header data-testid="email-field">
+        <section data-testid="user-email">
+          {' '}
+          {userEmail}
+        </section>
+        <section data-testid="total-field">
+          {INITIAL_VALUE}
+        </section>
+        <section data-testid="header-currency-field">
+          BRL
+        </section>
+      </header>
+    </>
   );
 }
 
